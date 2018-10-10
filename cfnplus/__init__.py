8ce0a10559a22e295722a241fafae5f239b6c686
C. Dylan Shearer <charles.shearer@hpe.com>
2018-10-10 16:12:37 -0700

    Add copyright statement to each source file

--- cfnplus/__init__.py
+++ cfnplus/__init__.py
@@ -1,3 +1,16 @@
+# (C) Copyright 2018 Hewlett Packard Enterprise Development LP.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# and in the "LICENSE.txt" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
 # pylint: disable=superfluous-parens
 # pylint: disable=invalid-name
 # pylint: disable=missing-docstring
